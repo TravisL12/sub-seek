7bdff5f8f77b4d5762b24ccdc115b7517864dce9
Travis Lawrence <travis.lawrence12@gmail.com>
2024-01-15 22:54:39 -0800

    try catch background

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,19 +1,23 @@
 let isOpen = false;
 
 chrome.action.onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    if (isOpen) {
-      chrome.tabs.sendMessage(activeTab.id, {
-        message: 'closeSubSeek',
-      });
-    } else {
-      chrome.tabs.sendMessage(activeTab.id, {
-        message: 'openSubSeek',
-      });
-    }
-    isOpen = !isOpen;
-  });
+  try {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      if (isOpen) {
+        chrome.tabs.sendMessage(activeTab.id, {
+          message: 'closeSubSeek',
+        });
+      } else {
+        chrome.tabs.sendMessage(activeTab.id, {
+          message: 'openSubSeek',
+        });
+      }
+      isOpen = !isOpen;
+    });
+  } catch (err) {
+    console.log('SubSeek Background Script Error:', err);
+  }
 });
 
 chrome.runtime.onMessage.addListener(function (message) {
