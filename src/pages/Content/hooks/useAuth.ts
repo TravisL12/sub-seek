5680f033ec37577f84e2670c401770c44b61dea7
Travis Lawrence <travis@instabase.com>
2024-01-19 11:29:53 -0800

    fallback server

--- src/pages/Content/hooks/useAuth.ts
+++ src/pages/Content/hooks/useAuth.ts
@@ -9,11 +9,12 @@ import { TAuth, TServerDevice } from '../components/types';
 4. Set the auth with the URI and token from that server
 */
 
+const clientIdentifier = localStorage['clientID'];
+const localToken = localStorage['myPlexAccessToken'];
 export const useAuth = () => {
   const [servers, setServers] = useState<TServerDevice[]>();
   const [auth, setAuth] = useState<TAuth>();
   const [currentServerId, setCurrentServerId] = useState<string>();
-  const clientIdentifier = localStorage['clientID'];
 
   useEffect(() => {
     const popCb = () => {
@@ -26,10 +27,16 @@ export const useAuth = () => {
   }, [servers]);
 
   const getAuth = async (serverDevices: any) => {
-    const currentServer = serverDevices.find((device: any) => {
+    let currentServer = serverDevices.find((device: any) => {
       return location.hash.includes(device.remoteClientIdentifier);
     });
 
+    if (!currentServer) {
+      // if you can't match the hash, then find any server that has the local token
+      currentServer = serverDevices.find((device: any) => {
+        return device.token === localToken;
+      });
+    }
     const isNewServer =
       !currentServerId ||
       currentServer?.remoteClientIdentifier !== currentServerId;
